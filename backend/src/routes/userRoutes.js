f2f5bfc77bd5597cf68278afb443c215d2e403e8
sm11t <adatta18@asu.edu>
2025-04-06 08:19:37 -0700

    Delete backend directory

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const checkJwt = require('../middleware/verifyToken');
-const { getProfile } = require('../controllers/userController');
-
-// GET /users/profile
-router.get('/profile', checkJwt, getProfile);
-
-module.exports = router;
